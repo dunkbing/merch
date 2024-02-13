62c1b1ebe3c6996bd1edadc7e2050f48045ac971
Bùi Đặng Bình <dangbinh4869@gmail.com>
2024-02-13 16:31:28 +0000

    update details

--- islands/ProductDetails.tsx
+++ islands/ProductDetails.tsx
@@ -122,12 +122,10 @@ export default function ProductDetails({ product }: { product: Product }) {
             </div>
           )}
         </div>
-      </div>
-
-      {/* Product form */}
-      <div class="mt-12 lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start">
-        <div class="mt-4">
-          <AddToCart {...product} />
+        <div class="lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start">
+          <div class="mt-4 self-end">
+            <AddToCart {...product} />
+          </div>
         </div>
       </div>
     </div>
