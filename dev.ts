9840e1bedfb5192f449b130020aba006b63e6a89
binh.buidang <dangbinh4869@gmail.com>
2024-02-19 09:19:22 +0700

    update  config.

--- dev.ts
+++ dev.ts
@@ -1,12 +1,8 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
+import "$std/dotenv/load.ts";
+
 import dev from "$fresh/dev.ts";
 import config from "@/fresh.config.ts";
-import "$std/dotenv/load.ts";
-import { importCategories } from "@/utils/db/category.ts";
-import { triggerJobs } from "@/utils/cron_jobs.ts";
-
-// await importCategories();
-// await triggerJobs();
 
 await dev(import.meta.url, "./main.ts", config);
--- main.ts
+++ main.ts
@@ -9,10 +9,5 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "@/fresh.config.ts";
-import { triggerJobs } from "@/utils/cron_jobs.ts";
-import { importCategories } from "@/utils/db/category.ts";
-
-await importCategories();
-await triggerJobs();
 
 await start(manifest, config);
